06fe337136514b6cc7e0d4f0b907eebe3a793c97
Shubham <shubham@example.com>
2026-03-26 17:03:50 +0530

    chore(deploy): adapt docker-compose and frontend for native Nginx proxy port routing

--- frontend/src/lib/nakama.ts
+++ frontend/src/lib/nakama.ts
@@ -2,7 +2,7 @@ import { Client } from '@heroiclabs/nakama-js';
 
 const isProd = window.location.hostname !== '127.0.0.1' && window.location.hostname !== 'localhost';
 const host = isProd ? "api-tictactoe.startup-lab.cloud" : window.location.hostname;
-const port = isProd ? "443" : "7350";
-const USE_SSL = isProd || window.location.protocol === 'https:';
+const USE_SSL = window.location.protocol === 'https:';
+const port = isProd ? (USE_SSL ? "443" : "80") : "7350";
 
 export const client = new Client("defaultkey", host, port, USE_SSL);
