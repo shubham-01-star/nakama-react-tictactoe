edb7357d64b1f4a0011a8d59486b4dc83450b1f2
Shubham <shubham@example.com>
2026-03-26 17:16:33 +0530

    fix(deploy): rigidly force secure wss connection to api on non-localhost setups

--- frontend/src/lib/nakama.ts
+++ frontend/src/lib/nakama.ts
@@ -1,8 +1,8 @@
 import { Client } from '@heroiclabs/nakama-js';
 
 const isProd = window.location.hostname !== '127.0.0.1' && window.location.hostname !== 'localhost';
-const host = isProd ? "api-tictactoe.startup-lab.cloud" : window.location.hostname;
-const USE_SSL = window.location.protocol === 'https:';
-const port = isProd ? (USE_SSL ? "443" : "80") : "7350";
+const host = isProd ? "api-tictactoe.startup-lab.cloud" : "127.0.0.1";
+const USE_SSL = isProd ? true : false;
+const port = isProd ? "443" : "7350";
 
 export const client = new Client("defaultkey", host, port, USE_SSL);
