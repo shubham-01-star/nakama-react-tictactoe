dc72b6490c033888d58ac93a1e347cc2baef4cc9
Shubham <shubham@example.com>
2026-03-26 17:00:53 +0530

    fix: Update Nakama API and console domain names to use hyphens.

--- frontend/src/lib/nakama.ts
+++ frontend/src/lib/nakama.ts
@@ -1,7 +1,7 @@
 import { Client } from '@heroiclabs/nakama-js';
 
 const isProd = window.location.hostname !== '127.0.0.1' && window.location.hostname !== 'localhost';
-const host = isProd ? "api.tictactoe.startup-lab.cloud" : window.location.hostname;
+const host = isProd ? "api-tictactoe.startup-lab.cloud" : window.location.hostname;
 const port = isProd ? "443" : "7350";
 const USE_SSL = isProd || window.location.protocol === 'https:';
 
